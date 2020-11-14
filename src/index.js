5f47a9bec8b6b19a6be223e7c5cf57d6c24fab21
Milton Chandradas <milton.chandradas@sap.com>
2020-11-14 05:23:31 -0500

    service worker...

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import App from './App';
+import App from "./App";
+import { swDev } from "./swDev";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+   <React.StrictMode>
+      <App />
+   </React.StrictMode>,
+   document.getElementById("root")
 );
 
+swDev();
